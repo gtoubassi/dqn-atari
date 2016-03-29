75ffa1bc75b5f571aec46a8f88b09161db643342
Ubuntu <ubuntu@ip-10-199-91-164.us-west-1.compute.internal>
2016-03-29 00:11:45 +0000

    Add script to pull summary stats from the log of a training run

---
+++ logstats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -n "Average score first 50:  "
+grep Episode $1 | head -50 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score last 50:   "
+grep Episode $1 | tail -50 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score first 100: "
+grep Episode $1 | head -100 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score last 100:  "
+grep Episode $1 | tail -100 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score first 250: "
+grep Episode $1 | head -250 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score last 250:  "
+grep Episode $1 | tail -250 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score first 500: "
+grep Episode $1 | head -500 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score last 500:  "
+grep Episode $1 | tail -500 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo "Best scores ever:"
+grep Episode $1 | awk '{print $6}' | sort -n | tail -5 
+
