815086e1b0387a26dc0d1d73d3f727749adf4ff6
Garrick Toubassi <gtoubassi@gmail.com>
2016-03-30 15:58:48 -0700

    Add 1000 episode stats to lostats

--- logstats.sh
+++ logstats.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-echo -n "Number of episodes:      "
+echo -n "Number of episodes:       "
 grep Episode $1 | awk '{print $2}' | tail -1
 
-echo -n "Average score first 50:  "
+echo -n "Average score first 50:   "
 grep Episode $1 | head -50 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score last 50:   "
+echo -n "Average score last 50:    "
 grep Episode $1 | tail -50 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score first 100: "
+echo -n "Average score first 100:  "
 grep Episode $1 | head -100 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score last 100:  "
+echo -n "Average score last 100:   "
 grep Episode $1 | tail -100 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score first 250: "
+echo -n "Average score first 250:  "
 grep Episode $1 | head -250 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score last 250:  "
+echo -n "Average score last 250:   "
 grep Episode $1 | tail -250 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score first 500: "
+echo -n "Average score first 500:  "
 grep Episode $1 | head -500 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
-echo -n "Average score last 500:  "
+echo -n "Average score last 500:   "
 grep Episode $1 | tail -500 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score first 1000: "
+grep Episode $1 | head -1000 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
+
+echo -n "Average score last 1000:  "
+grep Episode $1 | tail -1000 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
 echo -n "Average score for ALL:  "
 grep Episode $1 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
