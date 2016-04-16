f353823f33a0ad6a24b0b2fc179c257aee06693d
Garrick Toubassi <gtoubassi@gmail.com>
2016-04-16 10:49:22 -0700

    Add frame count to stats

--- logstats.sh
+++ logstats.sh
@@ -3,6 +3,9 @@
 echo -n "Number of episodes:       "
 grep Episode $1 | awk '{print $2}' | tail -1
 
+echo -n "Number of frames:         "
+grep Episode train1.log | awk '{print $7}' | tr -d '(' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM}'
+
 echo -n "Average score first 50:   "
 grep Episode $1 | head -50 | awk '{print $6}' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{print SUM/COUNT}'
 
