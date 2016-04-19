46da6e4728fc2c41f7159ef08a14741c9e685485
Garrick Toubassi <gtoubassi@gmail.com>
2016-04-18 17:28:58 -0700

    graves rms from github.com/Jabberwockyll/deep_rl_ale

--- dqn.py
+++ dqn.py
@@ -110,8 +110,9 @@ class DeepQNetwork:
 
           # (??) learning rate
           #optimizer = GradientClippingOptimizer(tf.train.RMSPropOptimizer(learningRate, decay=.95, epsilon=.01))
-          optimizer = tf.train.RMSPropOptimizer(learningRate, decay=.95, epsilon=.01)
-          self.train_step = optimizer.minimize(self.loss)
+          #optimizer = tf.train.RMSPropOptimizer(learningRate, decay=.95, epsilon=.01)
+          #self.train_step = optimizer.minimize(self.loss)
+          self.train_step = self.buildGravesRMSPropOptimizer(learningRate, .95, .01, 0)
 
           self.saver = tf.train.Saver(max_to_keep=25)
 
@@ -193,3 +194,34 @@ class DeepQNetwork:
                     self.staleSess.close()
                 self.staleSess = tf.Session()
                 self.saver.restore(self.staleSess, savedPath)
+
+    # from github.com/Jabberwockyll/deep_rl_ale
+    def buildGravesRMSPropOptimizer(self, learning_rate, rmsprop_decay, rmsprop_constant, gradient_clip):
+
+        with tf.name_scope('rmsprop'):
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+
+            grads_and_vars = optimizer.compute_gradients(self.loss)
+            grads = [gv[0] for gv in grads_and_vars]
+            params = [gv[1] for gv in grads_and_vars]
+
+            if gradient_clip > 0:
+                grads = tf.clip_by_global_norm(grads, gradient_clip)[0]
+
+            square_grads = [tf.square(grad) for grad in grads]
+
+            avg_grads = [tf.Variable(tf.ones(var.get_shape())) for var in params]
+            avg_square_grads = [tf.Variable(tf.ones(var.get_shape())) for var in params]
+
+            update_avg_grads = [grad_pair[0].assign((rmsprop_decay * grad_pair[0]) + ((1 - rmsprop_decay) * grad_pair[1])) 
+                for grad_pair in zip(avg_grads, grads)]
+            update_avg_square_grads = [grad_pair[0].assign((rmsprop_decay * grad_pair[0]) + ((1 - rmsprop_decay) * tf.square(grad_pair[1]))) 
+                for grad_pair in zip(avg_square_grads, grads)]
+            avg_grad_updates = update_avg_grads + update_avg_square_grads
+
+            rms = [tf.sqrt(avg_grad_pair[1] - tf.square(avg_grad_pair[0]) + rmsprop_constant) for avg_grad_pair in zip(avg_grads, avg_square_grads)]
+
+            rms_updates = [grad_rms_pair[0] / grad_rms_pair[1] for grad_rms_pair in zip(grads, rms)]
+            train = optimizer.apply_gradients(zip(rms_updates, params))
+
+            return tf.group(train, tf.group(*avg_grad_updates))
