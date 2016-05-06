a7346579f7254f080f1ce3b294ee739ffe16537a
Garrick Toubassi <gtoubassi@gmail.com>
2016-05-05 20:43:00 -0700

    Add support for normalizing weights they way I *think* deepmind is doing it

--- dqn.py
+++ dqn.py
@@ -1,4 +1,5 @@
 import state
+import math
 import numpy as np
 import os
 import tensorflow as tf
@@ -30,7 +31,8 @@ class DeepQNetwork:
         self.baseDir = baseDir
         self.saveModelFrequency = args.save_model_freq
         self.targetModelUpdateFrequency = args.target_model_update_freq
-        
+        self.normalizeWeights = args.normalize_weights
+
         self.batchCount = 0
         self.staleSess = None
 
@@ -101,24 +103,21 @@ class DeepQNetwork:
 
         # Second layer convolves 32 8x8 filters with stride 4 with relu
         with tf.variable_scope("cnn1_" + name):
-            W_conv1 = tf.Variable(tf.truncated_normal([8, 8, 4, 32], stddev=0.01), trainable=trainable, name="W_conv1")
-            b_conv1 = tf.Variable(tf.fill([32], 0.1), trainable=trainable, name="b_conv1")
+            W_conv1, b_conv1 = self.makeLayerVariables([8, 8, 4, 32], trainable, "conv1")
 
             h_conv1 = tf.nn.relu(tf.nn.conv2d(x_normalized, W_conv1, strides=[1, 4, 4, 1], padding='VALID') + b_conv1, name="h_conv1")
             print(h_conv1)
 
         # Third layer convolves 64 4x4 filters with stride 2 with relu
         with tf.variable_scope("cnn2_" + name):
-            W_conv2 = tf.Variable(tf.truncated_normal([4, 4, 32, 64], stddev=0.01), trainable=trainable, name="W_conv2")
-            b_conv2 = tf.Variable(tf.fill([64], 0.1), trainable=trainable, name="b_conv2")
+            W_conv2, b_conv2 = self.makeLayerVariables([4, 4, 32, 64], trainable, "conv2")
 
             h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_conv2, strides=[1, 2, 2, 1], padding='VALID') + b_conv2, name="h_conv2")
             print(h_conv2)
 
         # Fourth layer convolves 64 3x3 filters with stride 1 with relu
         with tf.variable_scope("cnn3_" + name):
-            W_conv3 = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=0.01), trainable=trainable, name="W_conv3")
-            b_conv3 = tf.Variable(tf.fill([64], 0.1), trainable=trainable, name="b_conv3")
+            W_conv3, b_conv3 = self.makeLayerVariables([3, 3, 64, 64], trainable, "conv3")
 
             h_conv3 = tf.nn.relu(tf.nn.conv2d(h_conv2, W_conv3, strides=[1, 1, 1, 1], padding='VALID') + b_conv3, name="h_conv3")
             print(h_conv3)
@@ -128,22 +127,32 @@ class DeepQNetwork:
 
         # Fifth layer is fully connected with 512 relu units
         with tf.variable_scope("fc1_" + name):
-            W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 512], stddev=0.01), trainable=trainable, name="W_fc1")
-            b_fc1 = tf.Variable(tf.fill([512], 0.1), trainable=trainable, name="b_fc1")
+            W_fc1, b_fc1 = self.makeLayerVariables([7 * 7 * 64, 512], trainable, "fc1")
 
             h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1, name="h_fc1")
             print(h_fc1)
 
         # Sixth (Output) layer is fully connected linear layer
         with tf.variable_scope("fc2_" + name):
-            W_fc2 = tf.Variable(tf.truncated_normal([512, numActions], stddev=0.01), trainable=trainable, name="W_fc2")
-            b_fc2 = tf.Variable(tf.fill([numActions], 0.1), trainable=trainable, name="b_fc2")
+            W_fc2, b_fc2 = self.makeLayerVariables([512, numActions], trainable, "fc2")
 
             y = tf.matmul(h_fc1, W_fc2) + b_fc2
             print(y)
             
         return x, y
 
+    def makeLayerVariables(self, shape, trainable, name_suffix):
+        if self.normalizeWeights:
+            # This is my best guess at what DeepMind does via torch's Linear.lua and SpatialConvolution.lua (see reset methods).
+            # np.prod(shape[0:-1]) is attempting to get the total inputs to each node
+            stdv = 1.0 / math.sqrt(np.prod(shape[0:-1]))
+            weights = tf.Variable(tf.random_uniform(shape, minval=-stdv, maxval=stdv), trainable=trainable, name='W_' + name_suffix)
+            biases  = tf.Variable(tf.random_uniform([shape[-1]], minval=-stdv, maxval=stdv), trainable=trainable, name='W_' + name_suffix)
+        else:
+            weights = tf.Variable(tf.truncated_normal(shape, stddev=0.01), trainable=trainable, name='W_' + name_suffix)
+            biases  = tf.Variable(tf.fill([shape[-1]], 0.1), trainable=trainable, name='W_' + name_suffix)
+        return weights, biases
+        
     def inference(self, screens):
         y = self.sess.run([self.y], {self.x: screens})
         q_values = np.squeeze(y)
