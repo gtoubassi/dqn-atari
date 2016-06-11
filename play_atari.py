3c159fe071ba5404b37a28ea02cf4c26966132f5
Garrick Toubassi <gtoubassi@gmail.com>
2016-06-11 12:36:23 -0700

    Save model every 10k vs 1k.  Was a bit too frequent

--- play_atari.py
+++ play_atari.py
@@ -19,7 +19,7 @@ parser.add_argument("--prioritized-replay", action='store_true', help="Prioritiz
 parser.add_argument("--compress-replay", action='store_true', help="if set replay memory will be compressed with blosc, allowing much larger replay capacity")
 parser.add_argument("--normalize-weights", action='store_true', default=True, help="if set weights/biases are normalized like torch, with std scaled by fan in to the node")
 parser.add_argument("--screen-capture-freq", type=int, default=250, help="record screens for a game this often")
-parser.add_argument("--save-model-freq", type=int, default=1000, help="save the model once per 1000 training sessions")
+parser.add_argument("--save-model-freq", type=int, default=10000, help="save the model once per 10000 training sessions")
 parser.add_argument("--observation-steps", type=int, default=50000, help="train only after this many stesp (=4 frames)")
 parser.add_argument("--learning-rate", type=float, default=0.00025, help="learning rate (step size for optimization algo)")
 parser.add_argument("--target-model-update-freq", type=int, default=10000, help="how often (in steps) to update the target model.  Note nature paper says this is in 'number of parameter updates' but their code says steps. see tinyurl.com/hokp4y8")
